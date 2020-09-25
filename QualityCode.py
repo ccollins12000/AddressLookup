78d8052e9aaca38f74c6168f3c6ac84fa263f88c
Charles Collins <coll5631@stthomas.edu>
2020-09-25 13:50:42 -0500

    Added confidence code

--- QualityCode.py
+++ QualityCode.py
@@ -62,3 +62,34 @@ granularity_lookup = {
         'Description': 'Postal code, smallest. Unused in USA.'
     },
 }
+
+confidence_lookup = {
+    'A': {
+        'Confidence': 'Exact',
+        'Description': 'Match locations that exactly correspond to the input location, as defined by the address elements specified.',
+        'StreetCon': 'Exact matches, although the geocoding engine allows standard variations of road types, directionals, numbered roads, and common abbreviations. Examples include Road vs. RD, North vs. N, Second vs. 2nd, and Mount vs. MT.',
+        'AdminCon': 'Exact matches, although common abbreviations are acceptable. For instance, "Mount View" is the same as "Mt. View."',
+        'PostalCon': 'The postal code matches at the granularity of the input. The matched postal code may be even more precise than the postal code specified in the input.'
+    },
+    'B': {
+        'Confidence': 'Good',
+        'Description': 'Matches should be fairly similar, even if not exactly as specified.',
+        'StreetCon': 'Good matches, ignoring differences in road type and directionals. EG: Road vs. Street,North vs. South. Includes matches where type & directional are supplied but not found, or found but not supplied.',
+        'AdminCon': 'Good matches, even if administrative area names do not exactly match. This includes sound-alike matches, partial, slight misspellings, or other fuzzy matching. Details vary by geocoding data.',
+        'PostalCon': 'Postal code does not exactly match at the granularity of the input. However, the postal code likely matches the input address at a lower granularity level than at the requested granularity.'
+    },
+    'C' : {
+        'Confidence': 'Approx',
+        'Description': 'Matches should be somewhat similar to the input location as specified.',
+        'StreetCon': 'Sound-alike matches, partial matches, slight misspellings, and fuzzy matching. Details vary by geocoding data.',
+        'AdminCon': 'Matched administrative areas do not match the input administrative areas. This may occur when the postal code input determines the match.',
+        'PostalCon': 'Postal codes do not match. This may occur when the administrative area input determines the match.'
+    },
+    'X' : {
+        'Confidence': 'None',
+        'Description': 'Confidence level has no meaning for this granularity level or was not used.',
+        'StreetCon': 'Confidence level has no meaning for this granularity level or was not used.',
+        'AdminCon': 'Confidence level has no meaning for this granularity level or was not used.',
+        'PostalCon': 'Confidence level has no meaning for this granularity level or was not used.'
+    }
+}
