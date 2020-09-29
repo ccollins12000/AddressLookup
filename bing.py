63d45b50530922e03aee9b239e2c7168a0e3d75f
Charles Collins <coll5631@stthomas.edu>
2020-09-28 20:45:55 -0500

    added initial geocoder for bing

---
+++ bing.py
@@ -0,0 +1,62 @@
+import requests
+
+class Geocoder:
+    """An object for searching for companies using google's custom search engine API
+
+    Attributes:
+    """
+    def __init__(self, api_key):
+        """The constructor for the company searcher
+
+        Args:
+            api_key (str): The secret key for accessing the api
+            api_app_key (str): The id for the custom search engine
+        """
+        self._session = requests.session()
+        self._response = None
+        self._api_key = api_key
+        self.address = None
+        self.result = None
+
+    def search(self, address):
+        """Searches for a company
+
+        Args:
+            address (str): The full address of the company
+        Raises:
+            Exception: If an invalid Bing key is provided
+        """
+        self.address = address
+
+        # Conduct search
+        # search_api_url = 'http://open.mapquestapi.com/geocoding/v1/address'
+        search_api_url = 'http://dev.virtualearth.net/REST/v1/Locations'
+        params = {'key': self._api_key, 'q': self.address}
+        self._response = self._session.get(search_api_url, params=params)
+
+        # Parse results
+        #results_data = json.loads(self._response.text)
+        #self.result = AddressResult(results_data)
+
+    def write_results(self, write_path):
+        """Writes the json response of the google search to a text file
+
+        Args:
+            The path to write the file to
+        """
+        with open(write_path + '.json', 'w') as f:
+            f.write(self._response.text)
+
+if __name__ == "__main__":
+    api_key = input('Enter the bing api key: ')
+    write_path = input('Enter an output path for the response: ')
+    geocoder = Geocoder(api_key)
+    geocoder.search('1 Microsoft Way, Redmond Washington')
+    geocoder.write_results(write_path)
+
+
+
+
+
+
+#http://dev.virtualearth.net/REST/v1/Locations?q=100%20Main%20St.%20Somewhere,%20WA%2098001&key={BingMapsAPIKey}
